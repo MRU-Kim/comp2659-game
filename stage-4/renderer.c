f71ed9dea007488fb83a22dc89894535b5b41ac6
kcari843 <kcari843@mtroyal.ca>
2025-03-10 15:59:15 -0600

    started stage 4

---
+++ stage-4/renderer.c
@@ -0,0 +1,31 @@
+/*
+Names 			Talah Al-Zamel, Ethan Sigfusson, Kim Carino 
+Course name   	COMP 2659-002
+Stage	    	Stage 3
+Deadline        February 28, 2024
+File name       renderer.c
+Professor     	Steve Kalmar
+*/
+
+/*
+Contains rendering functions
+*/
+
+#include "renderer.h"
+#include "RASTER.H"
+
+void render(const DinoPlayer *player, UINT8 *base){
+    /* */
+}
+
+void dinoRender(const DinoPlayer *dino, UINT8 *base){
+    /* */
+}
+
+void obstacleRender(const CactusSmall *cactusSmall, UINT8 *base){
+    /* */
+}
+
+void groundRender(const Ground *ground, UINT8 *base){
+    /* */
+}
