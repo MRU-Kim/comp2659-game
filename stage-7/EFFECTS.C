9806336de06f8849947a89ce7e95b2391068c7fd
kim <112977641+MRU-Kim@users.noreply.github.com>
2025-03-17 01:07:39 -0600

    cleaned functions

--- stage-7/EFFECTS.C
+++ stage-7/EFFECTS.C
@@ -1,1 +1,15 @@
-/* SOUND EFFECTS MODULE */
+/*
+-----------------------------------------------------------------------------
+Names           Talah Al-Zamel, Ethan Sigfusson, Kim Carino
+Course name     COMP 2659-002
+Stage           Stage 7
+Deadline        March 17, 2024
+File name       EFFECTS.C
+Professor       Steve Kalmar
+-----------------------------------------------------------------------------
+*/
+
+#include "PSG.H"
+#include "EFFECTS.H"
+#include "../stage-2/CONST.H"
+
--- stage-7/PSG.C
+++ stage-7/PSG.C
@@ -4,20 +4,24 @@ Names           Talah Al-Zamel, Ethan Sigfusson, Kim Carino
 Course name     COMP 2659-002
 Stage           Stage 7
 Deadline        March 17, 2024
-File name       events.c
+File name       PSG.C
 Professor       Steve Kalmar
 -----------------------------------------------------------------------------
 */
 
-#include "psg.h"
+#include "PSG.H"
 
 /*
 -------------------------------------------------------------------
- function: write_psg
- writes to the given PSG registers with the given value
- input:
-    reg  -  value of PSG register
-    val  -  value to be written to the given register
+    function: write_psg
+
+    Writes to the given PSG registers with the given value.
+
+    input:
+        reg  -  value of PSG register.
+        val  -  value to be written to the given register.
+    output:
+        Void.
 -------------------------------------------------------------------
 */
 UINT8 value = 0x3F;
@@ -38,10 +42,12 @@ void write_psg(int reg, UINT8 val) {
 }
 /*
 -------------------------------------------------------------------
- function: read_psg
- -
- input:
-    reg  -  value of PSG register
+    function: read_psg
+    -
+    input:
+        reg  -  value of PSG register.
+    output:
+        Void.
 -------------------------------------------------------------------
 */
 UINT8 read_psg(int reg) {
@@ -51,11 +57,15 @@ UINT8 read_psg(int reg) {
 
 /*
 -------------------------------------------------------------------
- function: set_tone
- sets the given channel's tone with the given tuning value
- input:
-    channel  -   valid value of channel
-    tuning   -   12-bit tuning value
+    function: set_tone
+
+    Sets the given channel's tone with the given tuning value.
+
+    input:
+        channel  -   valid value of channel
+        tuning   -   12-bit tuning value
+    output:
+        Void.
 -------------------------------------------------------------------
 */
 void set_tone(int channel, int tuning) {
@@ -79,10 +89,14 @@ void set_tone(int channel, int tuning) {
 
 /*
 -------------------------------------------------------------------
- function: set_noise
- initializes the game's Plane struct with starting properties 
- input:
-    model  -   pointer to the game's Model struct
+    function: set_noise
+
+    Initializes the game's Plane struct with starting properties 
+
+    input:
+        model  -   pointer to the game's Model struct
+    output:
+        Void.
 -------------------------------------------------------------------
 */
 void set_noise(int tuning) {
@@ -93,6 +107,21 @@ void set_noise(int tuning) {
     return;
 }
 
+/*
+-------------------------------------------------------------------
+    function: set_volume
+
+    Sets the volume level for a specified audio channel in the PSG
+    the volume value is passed as a paramenter, and the funcation
+    writes it to the corresponding PSG register.
+
+    input:
+        channel -   audio channel to adjust
+        volume  -   desired volume level
+    output:
+        Void.
+-------------------------------------------------------------------
+*/
 void set_volume(int channel, int volume) {
     
     write_psg(channel+ GOTO_VOLUME, volume);
@@ -102,6 +131,20 @@ void set_volume(int channel, int volume) {
     return;
 }
 
+/*
+-------------------------------------------------------------------
+    function: set_envelope
+
+    Configures the envelope and sustain level for the PSG. 
+    Writes to the PSG to set the envelope's shape and sustain.
+
+    input:
+        shape   -  envelope's shape
+        sustain -  The sustain level
+    output:
+        Void.
+-------------------------------------------------------------------
+*/
 void set_envelope(int shape, unsigned int sustain) {
     write_psg(1, 1);
 
@@ -110,6 +153,22 @@ void set_envelope(int shape, unsigned int sustain) {
     return;
 }
 
+/*
+-------------------------------------------------------------------
+    function: enable_channel
+
+    Enables a specified audio channel's tone and noise
+    in the PSG. Adjusts the mixer register to control the
+    tone or noise for the active channel.
+
+    input:
+        channel - audio channel.
+        tone_on - tone state (on/off)
+        noise_on - noise state (on/off)
+    output:
+        Void.
+-------------------------------------------------------------------
+*/
 void enable_channel(int channel, int tone_on, int noise_on) {
     /* UINT8 value = 0x3F; */
     if (tone_on == ON) {
@@ -124,7 +183,19 @@ void enable_channel(int channel, int tone_on, int noise_on) {
 
     return;
 }
-void stop_sound()
-{
+
+/*
+-------------------------------------------------------------------
+    function: stop_sound
+
+    End the sound.
+
+    input:
+        None.
+    output:
+        Void.
+-------------------------------------------------------------------
+*/
+void stop_sound(){
     write_psg(MIXER,0x3F);
 }
