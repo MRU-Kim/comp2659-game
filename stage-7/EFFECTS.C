90af963a3f0652e537fd23f5fa238cf176a99a22
kim <112977641+MRU-Kim@users.noreply.github.com>
2025-03-18 01:39:40 -0600

    started on set_noise and set_envelope

--- stage-7/EFFECTS.C
+++ stage-7/EFFECTS.C
@@ -14,10 +14,12 @@ Professor       Steve Kalmar
 #include "../stage-2/CONST.H"
 
 void set_noise(int tuning){
-
+    writePsg(6,tuning);
 }
 
 void set_envelope(int shape, unsigned int sustain){
-    
+    writePsg(11,sustain & 0xFF);
+    writePsg(12,(sustain>>8) & 0xFF);
+    writePsg(13,shape);
 }
 
--- stage-7/TST_PSG.C
+++ stage-7/TST_PSG.C
@@ -3,37 +3,36 @@
 
 int main() {
    
-int i;
+    int i;
 
-helpInitialize();
-startMusic();
-while(!Cconis()){
-    for(i=0; i <26;i++){
-        updateMusic();
-    }
     helpInitialize();
     startMusic();
-}
+    while(!Cconis()){
+        for(i=0; i <26;i++){
+            updateMusic();
+        }
+        helpInitialize();
+        startMusic();
+    }
 
-   /**   set_noise(1) ;
+   /*   set_noise(1) ;
 
     set_tone(CHANNEL_A, 0x1AC);
     set_tone(CHANNEL_B, 0x153);
     set_tone(CHANNEL_C, 0x11D);
 
 
-enable_channel(CHANNEL_A, ON, ON) ;    
- enable_channel(CHANNEL_B, ON, ON) ;
-     enable_channel(CHANNEL_C, ON, ON) ;    
+    enable_channel(CHANNEL_A, ON, ON) ;    
+    enable_channel(CHANNEL_B, ON, ON) ;
+    enable_channel(CHANNEL_C, ON, ON) ;    
 
-set_volume( CHANNEL_A, 10);
-set_volume( CHANNEL_A,30);
-set_volume( CHANNEL_A, 45);
+    set_volume( CHANNEL_A, 10);
+    set_volume( CHANNEL_A,30);
+    set_volume( CHANNEL_A, 45);
 
-while(!Cconis())
-;
-stop_sound();
-*/
+    while(!Cconis());
+    stop_sound();
+    */
      
     return 0;
 }
