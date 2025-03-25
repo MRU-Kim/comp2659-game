c0ec8e13c1c515f24eade64e389c56339a34609b
EthanSigfussonMRU <esigf646@mtroyal.ca>
2025-03-25 09:56:16 -0600

    time?

--- stage-5/dino.c
+++ stage-5/dino.c
@@ -1,5 +1,5 @@
 /*
-Names 			Talah Al-Zamel, Ethan Sigfusson, Kim Carino 
+Names 			Talah Al-Zamel, Ethan Sigfusson, Kim Carino
 Course name   	COMP 2659-002
 Stage	    	Stage 5
 Deadline        March 17, 2024
@@ -23,37 +23,52 @@ Professor     	Steve Kalmar
 #include "clock.h"
 #include "input.h"
 
-
 #include <osbind.h>
 #include <stdio.h>
 
 char input;
 
+
 int main()
 {
     Model gameModel;
+
     RenderTracker tracker;
 
-    UINT8 *base = Physbase(); /* 8-bit */
+
+    UINT8 *base = Physbase();
     modelInitialize(&gameModel);
     initTracker(&tracker);
-    forceDraw(&gameModel,&tracker,base);
+    forceDraw(&gameModel, &tracker, base);
+
+    
 
     input = NULL;
-
     while (input != '`')
     {
-        
+        UINT32 timeThen, timeNow, timeElapsed;
+
+        timeNow = get_time();
+        timeElapsed = timeNow - timeThen;
+
+
+
+        /*async*/
         input = Cnecin();
         if (input != ' ')
         {
             evKBInputHandle(&gameModel, input);
         }
-        
         input = NULL;
 
-        evModelUpdate(&gameModel);
-        redraw(&gameModel, &tracker, base);
+        /*sync*/
+        if (timeElapsed > 0)
+        {
+            evModelUpdate(&gameModel);
+            redraw(&gameModel, &tracker, base);
+            timeThen = timeNow;
+        }
+
     }
     return 0;
 }
