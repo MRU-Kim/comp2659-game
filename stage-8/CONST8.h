4b7bec5f8dc720097a87a3168a18cafd5c84ed08
EthanSigfussonMRU <esigf646@mtroyal.ca>
2025-04-02 22:10:41 -0600

    fixed?

--- stage-8/CONST8.h
+++ stage-8/CONST8.h
@@ -10,12 +10,6 @@ Professor     	Steve Kalmar
 #ifndef CONST8_H
 #define CONST8_H
 
-typedef unsigned char UINT8;
-typedef unsigned int  UINT16;
-typedef unsigned long UINT32;
-
-#define true 1
-#define false 0
 
 /* keyboard keys */
 #define UpKey 0x00480000L
@@ -23,12 +17,12 @@ typedef unsigned long UINT32;
 #define EnterKey 0x0072000DL
 
 /* plot strings */
-#define int welcomeX 200;
-#define int welcomeY 300;
-#define int player1X 200;
-#define int player1Y 200;
-#define int player2X 200;
-#define int player2Y 100;
+#define  welcomeX 200;
+#define  welcomeY 300;
+#define  player1X 200;
+#define  player1Y 200
+#define  player2X 200
+#define  player2Y 100
 
 
 
--- stage-8/splsh.c
+++ stage-8/splsh.c
@@ -1,14 +1,19 @@
+
 #include "../stage-2/RASTER.H"
 #include "../stage-2/objects.h"
 #include "CONST8.H"
 
+
+
 int main()
 {
-    UINT8 *base = Physbase();
+    
+    int *base = Physbase();
 
     Cnecin();
     clearScreen(base);
     Cnecin();
     welcomeScreen();
     Cnecin();
+    return 0;
 }
